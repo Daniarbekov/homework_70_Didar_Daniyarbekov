cca687abcdf911712018fe2006a361238a7ba971
Daniarbekov <daniarbekov@gmail.com>
2022-10-14 14:31:06 +0600

    Создано вью детального просмотра TaskDetailView
    создан шаблон
    добавлена ссылка на страницу
    в urls добавлен путь страницы

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from webapp.views.index import IndexView
-from webapp.views.task import TaskCreateView
+from webapp.views.task import TaskCreateView, TaskDetailView
 
 urlpatterns = [
     path("", IndexView.as_view(), name='index'),
-    path('task/create', TaskCreateView.as_view(), name='task_create')
+    path('task/create', TaskCreateView.as_view(), name='task_create'),
+    path('task/<int:pk>/detail', TaskDetailView.as_view(), name='task_detail')
 ]
--- source/webapp/views/task.py
+++ source/webapp/views/task.py
@@ -1,7 +1,14 @@
 from django.views.generic.edit import CreateView
+from django.views.generic import DetailView
 from webapp.forms import TaskForm
+from webapp.models import Task
 
 
 class TaskCreateView(CreateView):
     template_name = 'task_create.html'
     form_class = TaskForm
+
+
+class TaskDetailView(DetailView):
+    model = Task
+    template_name = 'task_detail.html'
