07eb528381a75490f68fc7ef7b4ae8d42b9f3dee
Daniarbekov <daniarbekov@gmail.com>
2022-11-22 17:38:25 +0600

    Task Detail
    добавил вью TaskDetail
    добавил путь в urls

--- source/api/urls.py
+++ source/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from api.views import ProjectDetailView
+from api.views import ProjectDetailView, TaskDetailView
 
 
 urlpatterns = [
     path('project/<int:pk>', ProjectDetailView.as_view()),
+    path("task/<int:pk>", TaskDetailView.as_view()),
 ]
--- source/api/views.py
+++ source/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from api.serializators import ProjectSerializator
-from webapp.models import Project
+from api.serializators import ProjectSerializator, TaskSerializator
+from webapp.models import Project, Task
 from django.shortcuts import get_object_or_404
 
 
@@ -9,4 +9,11 @@ class ProjectDetailView(APIView):
     def get(self, request, pk, format=None):
         project = get_object_or_404(Project,pk=pk)
         serializer = ProjectSerializator(project)
+        return Response(serializer.data)
+
+
+class TaskDetailView(APIView):
+    def get(self, request, pk, format=None):
+        task = get_object_or_404(Task,pk=pk)
+        serializer = TaskSerializator(task)
         return Response(serializer.data)
